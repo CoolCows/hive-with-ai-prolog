daaca33825c433993bad51186d2e066bc5a16599
RodroVMS <democlesvanshepars@gmail.com>
2021-11-02 08:58:06 -0400

    update gui code structure

---
+++ gui/primitives/draw_hex.pl
@@ -0,0 +1,15 @@
+:-use_module(library(pce)).
+
+draw_hexagon(H) :- draw_hexagon(0,0,1,H).
+draw_hexagon(S, H) :- draw_hexagon(0,0,S,H).
+draw_hexagon(X, Y, H) :- draw_hexagon(X,Y,1,H).
+draw_hexagon(X, Y, S, H) :-
+    new(H, path),
+    send(H, append, point(X + -25*S, Y + -50*S)),
+    send(H, append, point(X + -50*S, Y)),
+    send(H, append, point(X + -25*S, Y + 50*S)),
+    send(H, append, point(X + 25*S, Y + 50*S)),
+    send(H, append, point(X + 50*S, Y)),
+    send(H, append, point(X + 25*S, Y + -50*S)),
+    send(H, append, point(X + -25*S, Y + -50*S)).
+
