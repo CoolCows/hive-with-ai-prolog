18d8321ad1433b1840fa73b4fec1b8cc8e07cbca
RodroVMS <democlesvanshepars@gmail.com>
2021-11-02 09:57:58 -0400

    small fix loading modules in start.pl

--- gui/start.pl
+++ gui/start.pl
@@ -1,9 +1,6 @@
-% Load scripts
-?- ["./primitives/draw_hex"].
-
 % Load libraries and tools
 :- use_module(library(pce)).
-:- use_module(draw_hex, [draw_hexagon/3]).
+:- use_module("./primitives/draw_hex", [draw_hexagon/3]).
 
 % Start Main Window
 ?- new(@p, picture('Board')).
