eb8388318b5954bb9299cbbde98a137be91727db
RodroVMS <democlesvanshepars@gmail.com>
2021-11-02 12:08:55 -0400

    add closing and changing game type to gui

--- gui/start.pl
+++ gui/start.pl
@@ -3,6 +3,29 @@
 :- use_module("./primitives/draw_hex", [draw_hexagon/3]).
 
 % Start Main Window
-?- new(@p, picture('Board')).
-?- draw_hexagon(500, 500, H), send(@p, display, H).
-?- send(@p, open).
+gui_init() :-
+    new(MainWin, frame("CoolCows' Hive Game")),
+    send(MainWin, append, new(Board, picture("Board"))),
+    send(new(D, dialog),above(Board)),
+    send(D, append, button(change_game,
+                                and(message(@prolog, game_type_selection()),
+                                    message(MainWin, destroy)))),
+    send(D, append, button(exit, message(MainWin, destroy))),
+    send(MainWin, open).
+
+game_type_selection() :-
+    new(S, frame("Choose Playstyle")),
+    send(S, append, new(D, dialog)),
+    send(D, append, new(BTS, dialog_group(buttons, group))),
+    send(BTS, gap, size(20,0)),
+    send(BTS, append, button(local_game,
+                        and(message(@prolog,gui_init()),
+                            message(S, destroy)))),
+    send(BTS, append, button(vs_ai)),
+    send(BTS, append, button(online_game)),
+    send(BTS, append, button(exit, message(S, destroy))),
+    send(BTS, layout_dialog),
+    send(BTS, open).
+
+% ?- game_type_selection().
+?- gui_init().
