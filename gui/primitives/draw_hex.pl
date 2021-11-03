707a4357aebd9e12080eafb4fdd461fb4e9a4bb2
RodroVMS <democlesvanshepars@gmail.com>
2021-11-03 14:45:32 -0400

    delete useless draw_hexagon methods definition

--- gui/primitives/draw_hex.pl
+++ gui/primitives/draw_hex.pl
@@ -1,11 +1,7 @@
-:- module(draw_hex, [ draw_hexagon/1,
-                      draw_hexagon/2,
-                      draw_hexagon/3,
-                      draw_hexagon/4]).
+:- module(draw_hex, [draw_hexagon/3,
+                     draw_hexagon/4]).
 :- use_module(library(pce)).
 
-draw_hexagon(H) :- draw_hexagon(0,0,1,H).
-draw_hexagon(S, H) :- draw_hexagon(0,0,S,H).
 draw_hexagon(X, Y, H) :- draw_hexagon(X,Y,1,H).
 draw_hexagon(X, Y, S, H) :-
     new(H, path),
