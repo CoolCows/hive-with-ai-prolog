6d0fdee394bb5afcdee97223cc2b83763013e64e
RodroVMS <democlesvanshepars@gmail.com>
2021-11-03 21:23:30 -0400

    create a graphic_cell class in xpce I'm going to use a more object oriented approach to gain in modularity and confort while coding.

--- gui/board/board_graphics.pl
+++ gui/board/board_graphics.pl
@@ -1,28 +1,38 @@
 :- module(board_graphics, [draw_board/2,
                        draw_board/3]).
 
+:- use_module(library(pce)).
 :- use_module("../primitives/draw_hex", [draw_hexagon/4]).
 :- use_module("../../game/cell", [init_cell/6,
                                   get_bug_type/2,
                                   get_row/2,
                                   get_col/2,
                                   get_stack_pos/2]).
+:- use_module(tools, [slope/5,
+                      trace/4,
+                      below_line/4,
+                      above_line/4]).
 
 draw_board(Board, Picture) :-
     draw_board(Board, Picture, 1).
 
 draw_board(_, Picture, S) :-
+    print('Drawing Board'),
     send(Picture, clear),
     get(Picture, height, H),
     get(Picture, width, W),
     MX is W/2, MY is H/2,
     get_test_cells(C),
-    %draw_tiles(C, Picture, S, MX, MY). 
-    draw_pos_moves(C, Picture, S, MX, MY).
+    print('Obtained Test Cells'),
+    draw_tiles(C, Picture, S, MX, MY). 
+    %draw_pos_moves(C, Picture, S, MX, MY).
     
 draw_tiles([],_,_,_,_).
 draw_tiles([Cell|Rest], Picture, S, MX, MY) :-
-    get_coordinates(Cell, MX, MY, S, X, Y), 
+    print('Drawing Tile'),
+    get_coordinates(Cell, X, Y),
+    print('Coordinates gotten'),
+    %get_coordinates(Cell, MX, MY, S, X, Y), 
     draw_hexagon(X, Y, S, Hex),
     send(Picture, display, Hex),
     draw_tiles(Rest, Picture, S, MX, MY).
@@ -35,13 +45,20 @@ draw_pos_moves([Mov|Rest], Picture, S, MX, MY) :-
     send(Picture, display, Hex),
     draw_pos_moves(Rest, Picture, S, MX, MY).
 
+get_coordinates(Cell, X, Y) :-
+    print('Analyzing new'),
+    get(Cell, position, Pos),
+    get(Pos, x, X),
+    get(Pos, y, Y).
+
 get_coordinates(Cell, MX, MY, S, X, Y) :-
+    print('Analyzing old'),
     get_col(Cell, Xindex),
     get_row(Cell, Yindex),
     X is MX + S*75*Xindex,
     Y is MY + S*100*Yindex + S*50*(Xindex mod 2).
 
-get_test_cells(C) :-
+get_test_cells([X]) :-
     init_cell(0, 0, 0, 0, 0, C1),
     init_cell(0, 0, 1, 0, 0, C2),
     init_cell(0, 0, 2, 0, 0, C3),
@@ -57,6 +74,39 @@ get_test_cells(C) :-
     init_cell(0, 1, -1, 0, 0, C13),
     init_cell(0, 1, -2, 0, 0, C14),
     init_cell(0, 1, -3, 0, 0, C15),
-    append(C, [], [C1, C2, C3, C4, C5]).%, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15]).
+    append(CF, [], [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15]),
+    new(X, graphic_cell(bug, point(0,0), point(0,0))).
 
 
+:- pce_begin_class(graphic_cell, object).
+
+variable(type, name, both, 'Specify cell bug or if it si postion is empty').
+variable(position, point, both, 'Indicates the center location of the hexagon').
+variable(center, point, both, 'Indicates the center of the board').
+variable(selected, bool, both, 'True if the user select this cell').
+variable(color, name, both, 'Which player owns this cell').
+
+initialise(C, Type:name, Position:point, Center:point) :->
+    send(C, type, Type),
+    send(C, position, Position),
+    send(C, center, Center),
+    send(C, selected, false).
+
+click_inside(C, ClickPosition:point, Result:bool) :->
+    get(ClickPosition, x, CX), get(ClickPosition, y, CY),
+    get(C, center, Center),get(Center, x, X),get(Center, y, Y),
+    Dist is 50,  
+    CY < Y + Dist,
+    CY > Y - Dist,
+    line(point(X + Dist/2, Y + Dist), point(X + Dist, Y), M1, N1),
+    below_line(ClickPosition, M1, N1).
+
+
+
+select(C) :->
+    send(C, selected, true). 
+
+deselect(C) :->
+    send(C, selected, false).
+
+:- pce_end_class.
