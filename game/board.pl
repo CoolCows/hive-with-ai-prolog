0a07c9ecb547372a426c3440e9dcb8f0ea6097f3
Adrian Portales <adrianportales135@gmail.com>
2021-11-05 14:53:26 -0400

    Add move_cell predicate and rename add_cell to add_new_cell in board.pl

--- game/board.pl
+++ game/board.pl
@@ -16,14 +16,15 @@
 					adjacent_cell_4/3,
 					adjacent_cell_5/3,
 					adjacent_cell_6/3,
-					add_cell/3
-					
+					add_new_cell/3,
+					move_cell/4
 					]).
 
 :- use_module( cell, [ get_row/2,
 					   get_col/2,
 					   init_cell/6,
-					   get_bug_type/2] 
+					   get_bug_type/2, 
+					   set_bug_type/3]
 				   ).
 :- use_module( utils, [push/3] ).
 :- use_module( player, [init_player/1] ).
@@ -135,8 +136,23 @@ adjacent_cells(Cell,Board, AdjCell):-
 %---------------------------------------------------------------%
 %---------------------------------------------------------------%
 
-add_cell(Cell,Board, NewBoard) :-
-	%TODO: validate position of new cell
+move_cell(SourceCell, DestCell, Board, NewBoard):-
+	get_cells(Board,Cells),
+	delete(Cells, SourceCell, NewCells),
+	move_cell_(SourceCell, DestCell, NewCells, NewCells1),
+	set_cells(NewCells1, Board, NewBoard).
+
+move_cell_(cell(BugType, _, _, Color,_), cell(none,Row, Col,_,_),Cells, NewCells):-
+	init_cell(BugType, Row, Col, Color,0,NewCell),
+	!,
+	push(NewCell, Cells,NewCells).
+	
+move_cell_(cell(BugType,_,_,Color,_), cell(_,Row,Col,_,StackPos),Cells, NewCells):-
+	NewStackPos is StackPos + 1,
+	init_cell(BugType,Row,Col,Color, NewStackPos, NewCell),
+	push(NewCell, Cells, NewCells).
+
+add_new_cell(Cell,Board, NewBoard) :-
 	get_cells(Board,BoardCells),
 	push(Cell, BoardCells, NewBoardCells),
 	set_cells(NewBoardCells,Board,NewBoard).
