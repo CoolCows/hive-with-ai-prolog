45f4c67a1c26de2c9791c6d19ec605ecac95fa40
RodroVMS <democlesvanshepars@gmail.com>
2021-11-09 23:50:37 -0500

    Display bug in hexagons accordingly

--- gui/graphics/board_graphics.pl
+++ gui/graphics/board_graphics.pl
@@ -33,9 +33,21 @@ draw_cells([Cell|Rest], Canvas, point(CW, CH), Scale) :-
     X is CW + Scale*75*Col,
     Y is CH + Scale*100*Row + Scale*50*(Col mod 2),
     get_hexagon(X, Y, Scale, Hexagon),
-    %T0d0: Modify the hexagon so it represents correctly the bug
+    %T0d0: Modify the hexagon so it represents correctly the player and bug
     send(Canvas, display, Hexagon),
+    draw_bug(Cell, X, Y, Canvas),
     draw_cells(Rest, Canvas, point(CW, CH), Scale). 
+
+draw_bug(Cell, X, Y, Canvas) :-
+    get_bug_type(Cell, Bug),
+    (
+        (Bug = queen, new(BM, bitmap( './graphics/xpm/queen.xpm')));
+        (Bug = ant, new(BM, bitmap('./graphics/xpm/ant.xpm')));
+        (Bug = beetle, new(BM, bitmap('./graphics/xpm/beetle.xpm')));
+        (Bug = grasshopper, new(BM, bitmap('./graphics/xpm/grasshopper.xpm')));
+        (Bug = spyder, new(BM, bitmap('./graphics/xpm/spyder.xpm')))
+    ),
+    send(Canvas, display, BM, point(X - 35, Y - 35)).
     
 get_hexagon(X, Y, H) :- draw_hexagon(X,Y,1,H).
 get_hexagon(X, Y, S, H) :-
@@ -50,21 +62,21 @@ get_hexagon(X, Y, S, H) :-
 
 % Erase method when complete
 get_test_cells(C) :-
-    init_cell(bug, 0, 0, black, 0, C1),
-    init_cell(bug, 0, 1, black, 0, C2),
-    init_cell(bug, 0, 2, white, 0, C3),
-    init_cell(bug, 0, 3, white, 0, C4),
-    init_cell(bug, 1, 0, white, 0, C5),
-    init_cell(bug, 1, 1, white, 0, C6),
-    init_cell(bug, 1, 2, white, 0, C7),
-    init_cell(bug, 1, 3, white, 0, C8),
-    init_cell(bug, 0, -1, white, 0, C9),
-    init_cell(bug, 0, -2, white, 0, C10),
-    init_cell(bug, 0, -3, white, 0, C11),
-    init_cell(bug, -1, 0, white, 0, C12),
-    init_cell(bug, 1, -1, white, 0, C13),
-    init_cell(bug, 1, -2, white, 0, C14),
-    init_cell(bug, 1, -3, white, 0, C15),
+    init_cell(ant, 0, 0, black, 0, C1),
+    init_cell(spyder, 0, 1, black, 0, C2),
+    init_cell(queen, 0, 2, white, 0, C3),
+    init_cell(grasshopper, 0, 3, white, 0, C4),
+    init_cell(beetle, 1, 0, white, 0, C5),
+    init_cell(spyder, 1, 1, white, 0, C6),
+    init_cell(spyder, 1, 2, white, 0, C7),
+    init_cell(spyder, 1, 3, white, 0, C8),
+    init_cell(beetle, 0, -1, white, 0, C9),
+    init_cell(beetle, 0, -2, white, 0, C10),
+    init_cell(beetle, 0, -3, white, 0, C11),
+    init_cell(beetle, -1, 0, white, 0, C12),
+    init_cell(ant, 1, -1, white, 0, C13),
+    init_cell(ant, 1, -2, white, 0, C14),
+    init_cell(ant, 1, -3, white, 0, C15),
     append(C, [], [C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15]).
 
 
