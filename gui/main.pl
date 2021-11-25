bd7eaee8a0c662df157e74dcb2406159ee16c610
RodroVMS <democlesvanshepars@gmail.com>
2021-11-25 08:42:05 -0500

    minor refactor

--- gui/main.pl
+++ gui/main.pl
@@ -1,13 +1,15 @@
 % Load libraries and tools
 :- use_module(library(pce)).
 :- use_module("./graphics/board_graphics", [draw_board/2]).
-:- use_module("./graphics/side_board_graphics", [draw_side_board/2]).
+:- use_module("./graphics/side_board_graphics", [draw_side_board/3]).
 :- use_module("./events/board_events", [select_event/2]).
 
 
 menu_bar_setup(MainWin, Board, BlackCells, WhiteCells) :-
     send(new(D, dialog), above, Board),
     send(D, append, new(MB, menu_bar)),
+    send(D, background, colour(orange)),
+
     send(MB, append, new(File, popup(file))),
     send_list(File, append,
                 [new(Opt, popup(new_Game)),
@@ -21,15 +23,15 @@ file_dialog_setup(Board, BlackCells, WhiteCells) :-
     send(new(T, dialog), right, Board),
     send(T, ver_shrink, 100),
     send(T, ver_stretch, 100),
-    send(T, width, 512),
+    send(T, background, colour(orange)),
 
-    send(T, append, new(BlackCells, picture)),
-    send(BlackCells, width, 500),
-    send(BlackCells, height, 250),
+    send(T, append, new(BlackCells, window)),
+    send(BlackCells, width, 400),
+    send(BlackCells, height, 200),
 
-    send(T, append, new(WhiteCells, picture)),
-    send(WhiteCells, width, 500),
-    send(WhiteCells, height, 250),
+    send(T, append, new(WhiteCells, window)),
+    send(WhiteCells, width, 400),
+    send(WhiteCells, height, 200),
 
     send(button(refresh, message(@prolog, draw_board,[], Board)), below, WhiteCells).
 
@@ -39,8 +41,8 @@ start_game(Board, BlackCells, WhiteCells) :-
     nb_setval(move_cell, false),
     nb_setval(position_cell, false),
     nb_setval(player_turn, white),
-    draw_side_board([], BlackCells),
-    draw_side_board([], WhiteCells),
+    draw_side_board([], black, BlackCells),
+    draw_side_board([], white, WhiteCells),
     draw_board([], Board).
 
 gui_init :-
@@ -54,8 +56,8 @@ gui_init :-
                           message(@prolog, select_event, Board, @event?position))),
     file_dialog_setup(Board, BlackPlayer, WhitePlayer),
     menu_bar_setup(MainWin, Board, BlackPlayer, WhitePlayer),
-    send(MainWin, open),
-    draw_side_board([], BlackPlayer),
-    draw_side_board([], WhitePlayer).
+    send(MainWin, open).
+    %draw_side_board([], BlackPlayer),
+    %draw_side_board([], WhitePlayer).
 
 ?- gui_init.
