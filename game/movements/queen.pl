978b03b86fc7e2972bdf45f451cec4cb9febec15
RodroVMS <democlesvanshepars@gmail.com>
2021-12-02 14:20:09 -0500

    fix queen movement

--- game/movements/queen.pl
+++ game/movements/queen.pl
@@ -4,6 +4,7 @@
 :- use_module("../cell").
 :- use_module("../utils").
 
-valid_queen_movement(SourceCell, cell(none, Row, Col, none, 0)) :-
+valid_queen_movement(SourceCell, AdjCell) :-
     % delete_cell(SourceCell),
-    adjacent_cell(SourceCell, cell(none, Row, Col, none, 0)).
+    adjacent_cell(SourceCell, AdjCell),
+    AdjCell = cell(none, _, _, none, _).
