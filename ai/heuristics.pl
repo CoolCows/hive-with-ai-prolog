3fa6fd76db71a75a5f1d0b4d62b1dda041d05e64
adrian13579 <adrianportales135@gmail.com>
2021-12-07 23:36:43 -0500

    Create basics heuristics

--- ai/heuristics.pl
+++ ai/heuristics.pl
@@ -3,53 +3,59 @@
 :- use_module("../game/hive_api").
 :- use_module("../game/board").
 
-% Import adyacent cells
-% Import cells movement
 
-% heuristic(
-%     game_state(Board, Turns, LastMovedCells, FixedCells, Players),
-%     NextGameState) 
-% :-
-%     true.
-
-surround_queen( NextGameState):-
+surround_enemy_queen(move(cell(B1,R1,C1,D1,S1),cell(B2,R2,C2,D2,S2))):-
+	DestCell = cell(B1,R2,C2,D1,S2),
 	hive_current_player_color(Color),
 	oponent_color(Color,OponentColor),
-	hive_get_cell(cell(_,_,_,Color,_),Cell),
-	hive_get_possible_moves(Cell, PosMoves),
 	hive_get_cell(cell(queen,_,_,OponentColor,_),QueenCell),
-	adjacent_cell(QueenCell,QueenAdj),
-	member(QueenAdj,PosMoves),
-	hive_move_cell(Cell,QueenAdj),
-	hive_get_game_state(NextGameState).
+	adjacent_cell(QueenCell,DestCell).
 
-free_queen(NextGameState):-
+surround_enemy_queen(place(Cell)):-
+	% hive_current_player_color(Color),
+	% oponent_color(Color,OponentColor),
+	% hive_get_cell(cell(queen,_,_,OponentColor,_),QueenCell),
+	% adjacent_cell(QueenCell,Cell).
+	false.
+
+free_current_player_queen(cell(B1,R1,C1,D1,S1),cell(B2,R2,C2,D2,S2)):-
+	DestCell = cell(B1,R2,C2,D1,S2),
+	SourceCell = cell(B1,R1,C1,D1,S1),
 	hive_current_player_color(Color),
 	hive_get_cell(cell(queen,_,_,Color,_),QueenCell),
-	adjacent_cell(QueenCell,AdjCell),
-	get_color(AdjCell,Color),
-	hive_get_possible_moves(AdjCell,PosMoves),
-	member(PosMove,PosMoves),
-	not(adjacent_cell(QueenCell,PosMove)),
-	move_cell(AdjCell,PosMove),
-	hive_get_game_state(NextGameState).
+	adjacent_cell(QueenCell,SourceCell),
+	not(adjacent_cell(QueenCell,DestCell)).
+	
+free_current_player_queen(place(Cell)):-
+	hive_current_player_color(Color),
+	hive_get_cell(cell(queen,_,_,Color,_),QueenCell),
+	not(adjacent_cell(QueenCell,Cell)).
 
+block_current_player_bug(move(SourceCell,DestCell)):-
+	true.
 
-    
-block_bug() :-
-    % Get Enemy Bug that can surround Ally Queen
-    % Put Ally Bug in a position were Enemy Bug cause
-    % Hive disruption
-    true.
-    
-place_bug() :-
-    % Shuld this method go here?
-    true.
+block_current_player_bug(place(Cell)):-
+	true.
 
-% current_player_color(T,Color):-
-% 	is_even(T),
-% 	Color = white.
+block_enemy_bug(move(cell(B1,R1,C1,D1,S1),cell(B2,R2,C2,D2,S2))):-
+	DestCell = cell(B1,R2,C2,D1,S2),
+	SourceCell = cell(B1,R1,C1,D1,S1),
+	delete_cell(SourceCell),
+	hive_current_player_color(Color),
+	oponent_color(Color,OponentColor),
+	init_cell(DestCell),
+	adjacent_cell(DestCell,AdjCell),
+	get_color(AdjCell,OponentColor),
+	gui_get_possible_moves(AdjCell,[]),
+	delete_cell(DestCell),
+	init_cell(SourceCell).
 
-% current_player_color(T,Color):-
-% 	is_odd(T),
-% 	Color = black.
+block_enemy_bug(place(Cell)):-
+	hive_current_player_color(Color),
+	oponent_color(Color,OponentColor),
+	init_cell(Cell),
+	adjacent_cell(Cell,AdjCell),
+	get_color(AdjCell,OponentColor),
+	gui_get_possible_moves(AdjCell,[]),
+	delete_cell(Cell).
+
