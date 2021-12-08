a1666d9fb34776ddccbd97b47c19fdae90f687d6
RodroVMS <democlesvanshepars@gmail.com>
2021-12-07 19:55:36 -0500

    add aux_address to tree nodes, so they can be searched by moves made too

--- ai/node.pl
+++ ai/node.pl
@@ -1,8 +1,14 @@
 :- module(node, [
+    get_address/2,
+    get_type/2,
     get_times_explored/2,
     get_times_white_won/2,
     get_times_black_won/2,
-    get_node/2
+    force_find_node/4,
+    add_initial_node/0,
+    find_node_by_game_state/2,
+    find_node_by_edge_move/2,
+    keccak256/3
 ]).
 
 :- use_module(library(persistency)).
@@ -11,6 +17,7 @@
 :- persistent
     node(
         address:string,          % hash(parent_address + str(game_state))
+        auxiliar_address:string, % hash(parent_address + str(move that made new_state from old_state))
         parent_address:string,   
         game_state:list,         % [board, players, turns, last_played]
         node_type: atom,         % non_terminal | white_won | black_won | draw
@@ -25,54 +32,84 @@ load_tree_db :-
     db_attach('./tree_db.pl', []).
 
 % Properties:
-%
+
 get_address(
     node(Address, _, _, _, _, _, _, _),
     Address
 ). 
 
+get_aux_address(
+    node(_, AuxAddress, _, _, _, _, _, _),
+    AuxAddress
+).
+
 get_parent_address(
-    node(_, ParentAddress, _, _, _, _, _, _),
+    node(_, _, ParentAddress, _, _, _, _, _, _),
     ParentAddress
 ). 
 
+get_type(
+    node(_, _, _, Type, _, _, _, _, _),
+    Type
+).
+
+
 get_times_explored(
-    node(_, _, _, _, _, Explored, _, _),
+    node(_, _, _, _, _, _, Explored, _, _),
     Explored
 ).
 
 get_times_white_won(
-    node(_, _, _, _, _, _, WhiteWon, _),
+    node(_, _, _, _, _, _, _, WhiteWon, _),
     WhiteWon
 ).
 
 get_times_black_won(
-    node(_, _, _, _, _, _, _, BlackWon),
+    node(_, _, _, _, _, _, _, _, BlackWon),
     BlackWon
 ).
 
 % Methods:
-get_node(ParentAddress, GameState, NodeType, Node) :-
+force_find_node(ParentAddress, GameState, EdgeMove, NodeType, Node) :-
    keccak256(ParentAddress, GameState, NodeAddress),
    (
-        find_node(NodeAddress, Node);
-        add_node(
-            NodeAddres, ParentAddress, GameState, NodeType, Node
+        find_node_by_game_state(NodeAddress, Node);
+        (
+            keccak256(ParentAddress, EdgeMove, AuxAddress),
+            add_node(
+                NodeAddres, AuxAddress, ParentAddress, GameState, NodeType, Node
+            )
         )
+    ).
+
+add_initial_node:-
+    find_node_by_game_state(1, _);
+    assert_node(
+        1,
+        1,
+        0,
+        %define game state
+        non_terminal,
+        true,
+        0,
+        0,
+        0
     ).
 
 add_node(
     NodeAddress,
+    AuxAddress,
     ParentAddress, 
     GameState, 
     NodeType,
     node(
-        NodeAddress, ParentAddress, GameState, NodeType,
-        false, 0, 0, 0
+        NodeAddress, AuxAddress, ParentAddress, GameState,
+        NodeType, false, 0, 0, 0
     )
 ):-
     assert_node(
         NodeAddress, 
+        AuxAddress,
         ParentAddress, 
         GameState, 
         NodeType, 
@@ -83,10 +120,24 @@ add_node(
     ).
 
 % returns a node by it's address
-find_node(
+find_node_by_game_state(
     NodeAddress,
     node(
         NodeAddress, 
+        AuxAddress,
+        ParentAddress, 
+        GameState, 
+        Explored, 
+        WhiteWon, 
+        BlackWon
+    )
+).
+
+find_node_by_edge_move(
+    AuxAddress,
+    node(
+        NodeAddress, 
+        AuxAddress,
         ParentAddress, 
         GameState, 
         Explored, 
@@ -99,12 +150,13 @@ find_node(
 % 1. Finds node at NodeAddress
 % 2. Updates properties
 % 3. Returns ParentAddress
-update_node(NodeAddress, NewExplored, NewWhiteWon, NewBlackWon, ParentAddress) :-
+update_node(NodeAddress, NewExplored, NewWhiteWon, NewBlackWon) :-
     find_node(NodeAddress, Node),
-    Node = node(_, ParentAddress, GameState, NodeType, Explored, WhiteWon, BlackWon),
+    Node = node(_, AuxAddress, ParentAddress, GameState, NodeType, Explored, WhiteWon, BlackWon),
     retract_node(Node),
     assert_node(
         NodeAddres,
+        AuxAddress,
         ParentAddress,
         GameState,
         NodeType,
@@ -113,7 +165,10 @@ update_node(NodeAddress, NewExplored, NewWhiteWon, NewBlackWon, ParentAddress) :
         NewBlackWon
     ).
 
-keccak256(ParentAddress, GameState, Hash) :-
-    term_string(GameState, StrGameState),
-    string_concat(ParentAddress, StrGameState, Seed),
+% ParentAddress: str
+% Salt: term
+% Hash: str (returns)
+keccak256(ParentAddress, Salt, Hash) :-
+    term_string(Salt, StrSalt),
+    string_concat(ParentAddress, StrSalt, Seed),
     crypto_data_hash(Seed, Hash, [algorithm(sha3)]).
