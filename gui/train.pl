95af644c1e8a8772a61661306435fdf9f55c0bcd
RodroVMS <democlesvanshepars@gmail.com>
2021-12-10 01:08:03 -0500

    add stamina to train faster

--- gui/train.pl
+++ gui/train.pl
@@ -1,3 +1,4 @@
+:- use_module("../ai/ai.pl").
 :- use_module("../ai/ai_api").
 :- use_module("../ai/node").
 :- use_module("../game/hive_api").
@@ -17,9 +18,22 @@ train :-
     write_to_file(Status),
     halt.
 
+stamina :-
+    start_up_ia,
+    add_initial_node,
+    hive_init_players(),
+    hive_init_board(),
+
+    find_node_by_game_state('1',RootNode),
+    run_simulation(RootNode, N1),
+    run_simulation(N1, N2),
+    run_simulation(N2, N3),
+    run_simulation(N3, N4),
+    halt.
+
 write_to_file(Status):-
     open('./log.txt', append, File),
     write(File, Status),
     close(File).
 
-?- train.
+?- stamina.
